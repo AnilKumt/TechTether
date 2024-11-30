83595d084a7af3597f9bb5cf239391962e4980b9
AnilKumt <anilkumt51@gmail.com>
2024-11-30 13:27:59 +0530

    made signup API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {connectDB} = require("./config/database.js");
 const {adminAuth,userAuth} = require("./middlewares/auth.js");
+const {userModel} = require("./models/user.js");
 const app = express();
 
 connectDB()
@@ -13,6 +14,24 @@ connectDB()
     .catch((err)=>{
         console.log("DB connection failed "+err);
     })
+
+app.post("/signup",async (req,res)=>{
+    const newUser =  new userModel({
+        firstName:"Rahul",
+        lastName:"abc",
+        age:20,
+        emailId:"rahul123@mail.com"
+    });
+    try{
+        await newUser.save();
+        res.send("Congratulations!.You have signed Up !");
+    }
+    catch{
+        res.send(400).send("Sorry. Unable to signUp Now. Try after some time");
+    }
+
+});
+
 app.use("/admin",adminAuth,(req,res)=>{
     res.send("Admin is Verified");
 });
