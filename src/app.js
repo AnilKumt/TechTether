1f265a521ee2c48eea7ba928055ef0e373a6c140
AnilKumt <anilkumt51@gmail.com>
2024-11-30 13:38:31 +0530

    made signup API dynamic

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const {connectDB} = require("./config/database.js");
 const {adminAuth,userAuth} = require("./middlewares/auth.js");
 const {userModel} = require("./models/user.js");
 const app = express();
-
+// connecting to DB and then making server listen at port number 3000
 connectDB()
     .then(()=>{
         console.log("Connected to DB");
@@ -15,13 +15,18 @@ connectDB()
         console.log("DB connection failed "+err);
     })
 
+// middleware to convert JSON data into javascript object.
+app.use(express.json());
+
+// Signup API
 app.post("/signup",async (req,res)=>{
-    const newUser =  new userModel({
-        firstName:"Rahul",
-        lastName:"abc",
-        age:20,
-        emailId:"rahul123@mail.com"
-    });
+    // const newUser =  new userModel({
+    //     firstName:"Rahul",
+    //     lastName:"abc",
+    //     age:20,
+    //     emailId:"rahul123@mail.com"
+    // });
+    const newUser = new userModel(req.body);
     try{
         await newUser.save();
         res.send("Congratulations!.You have signed Up !");
